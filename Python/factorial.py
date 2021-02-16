246b73e03238036f5bee05ca07f1adf9633fa4d9
Rowan <shepherd.rowan.k@gmail.com>
2021-02-16 10:35:34 -0800

    Recursive  solution for factorial problem

--- Python/factorial.py
+++ Python/factorial.py
@@ -3,10 +3,17 @@ For example: if num = 4, then your program should return
 (4 * 3 * 2 * 1) = 24  '''
 
 
-#iterative solution
+# iterative solution
 
 def factorial(num):
 	result = num
     for i in range(1, num):
         result = result * i
     return result
+
+# recursive solution
+
+def factorial(num):
+    if num == 1:
+        return 1
+    return num * factorial(num-1)
