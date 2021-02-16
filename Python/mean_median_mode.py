16163941eb368d89238dd9cff2b96791dc3ce54f
Rowan <shepherd.rowan.k@gmail.com>
2021-02-16 12:04:58 -0800

    Mean median mode initial solutions

--- Python/mean_median_mode.py
+++ Python/mean_median_mode.py
@@ -1,3 +1,21 @@
 ''' Write a function to return the mean, median, and mode 
 	of a list of numbers.'''
 
+def mean(arr):
+    return sum(arr) / int(len(arr))
+
+def median(arr):
+    arr = sorted(arr)
+    return arr[int(len(arr)/2)]
+
+
+#only works if only single number is mode
+def mode(arr):
+    dict = {}
+    mode = 0
+    for item in arr:
+        dict[item] = dict.get(item,0)+1
+    for item in dict:
+        if dict[item] > mode:
+            mode = item
+    return mode
