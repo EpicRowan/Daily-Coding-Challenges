7cf49b88b74d33560fdfb0bfa5bb5b063c55f334
Rowan <shepherd.rowan.k@gmail.com>
2021-02-25 11:52:02 -0800

    Added solution and comments to rotate k

--- Python/rotate_k.py
+++ Python/rotate_k.py
@@ -2,3 +2,12 @@
 
 Input: nums = [1,2,3,4,5,6,7], k = 3
 Output: [5,6,7,1,2,3,4] '''
+
+def rotate(nums, k):
+	#rotates k rotations
+	for i in range(k):
+	previous = nums[-1]
+		#rotates end to front one rotation
+		for j in range(len(nums)):
+		nums[j], previous = previous, nums[j]
+	return nums
