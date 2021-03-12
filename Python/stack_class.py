b0abc4db92e4d5581e6bc074d243e06e0bafd627
Rowan <shepherd.rowan.k@gmail.com>
2021-03-12 09:52:23 -0800

    Updated stack class with linked list

--- Python/stack_class.py
+++ Python/stack_class.py
@@ -1,19 +1,36 @@
 ''' Outline a Stack class with any methods you
 would define'''
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class Stack:
-	
-	def __init__(self):
-		self.stack = []
-
-	def push(self, item):
-		self.stack.append(item)
-
-	def pop(self):
-		return self.stack.pop()
-
-	def peek(self):
-		return self.stack[-1]
-
-	def is_empty(self):
-		return not self.stack
+    def __init__(self, top = None, bottom = None):
+        self.top = top
+        self.bottom = bottom
+        self.length = 0
+    
+    def peek(self):
+        return self.top.value
+    
+    def push(self, value):
+        newNode = Node(value)
+        if self.length == 0:
+            self.top = newNode
+            self.bottom = newNode
+        else:
+            hold = self.top
+            self.top = newNode
+            self.top.next = hold
+        self.length +=1
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            hold = self.top
+            self.top = self.top.next
+            self.length -= 1
+        return hold
