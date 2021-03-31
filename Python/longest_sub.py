0f9df79099e60ee8a28eb9fe2c599d26ef1b53a8
Rowan <shepherd.rowan.k@gmail.com>
2021-03-30 19:09:12 -0700

    Adde bubble sort problem and solution

--- Python/longest_sub.py
+++ Python/longest_sub.py
@@ -1,4 +1,18 @@
-''' Given a string s, find the length of the longest substring without repeating characters.
+''' Given a string s, find the length of the longest substring without 
+repeating characters.
 
-'''
+Input: s = "abcabcbb"
+Output: 3'''
 
+def lengthOfLongestSubstring(s):
+    start = 0
+    maxLength = 0
+    usedChar = {}
+    for index,char in enumerate(s):
+        if char in usedChar and start <= usedChar[char]:
+            start = usedChar[char] + 1
+            
+        else:
+            maxLength = max(maxLength, index - start + 1)
+        usedChar[char] = index
+    return maxLength
