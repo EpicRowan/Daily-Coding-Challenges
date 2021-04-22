174468fe46d66150fd6c36e3c1eb5e29ae153a80
Rowan <shepherd.rowan.k@gmail.com>
2021-04-22 11:58:52 -0700

    Updated palindrone and linked list solutions

--- Python/merge_ll.py
+++ Python/merge_ll.py
@@ -2,6 +2,14 @@
 	as a new sorted list.'''
 
 def merge_ll(lst1, lst2):
-	current = self.head
+	dummy = current = self.head
 	while lst1 and lst2:
-
+        if lst1.value < lst2.value:
+            current.next = lst1
+            lst1 = lst1.next
+        else:
+            current.next = lst2
+            lst2 = lst2.next
+        current = current.next
+    current.next = lst1 or lst2
+    return dummy.next
--- Python/palindrome.py
+++ Python/palindrome.py
@@ -30,3 +30,27 @@ def isPalindrome(str):
 				start += 1
 				end -= 1
 	return True
+
+''' Follow-up: Check if a linked list is a palindrone'''
+
+def is_palindrome(ll):
+    fast = slow = ll.head
+    stack = []
+
+    while fast and fast.next:
+        stack.append(slow.value)
+        slow = slow.next
+        fast = fast.next.next
+
+    if fast:
+        slow = slow.next
+
+    while slow:
+        top = stack.pop()
+
+        if top != slow.value:
+            return False
+
+        slow = slow.next
+
+    return True
